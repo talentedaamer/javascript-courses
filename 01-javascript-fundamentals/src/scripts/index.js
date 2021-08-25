4621f985d1aead97230ed6fe568a0c6c38aec614
Aamer Shahzad <a.shahzad@id-ware.com>
2021-08-25 15:52:07 +0500

    rest operator

--- 01-javascript-fundamentals/src/scripts/index.js
+++ 01-javascript-fundamentals/src/scripts/index.js
@@ -4,4 +4,35 @@ if (process.env.NODE_ENV === 'development') {
   require('../index.html');
 }
 
-console.log('webpack starterkit');
+// CONSTANTS
+// con not overwrite
+// can not initialize without value
+
+// LET AND VAR
+console.log('>> var defined later >>', testVar);
+var testVar = 'Hello';
+
+console.log('>> let defined later >>', testLet); // should give error, but code is compiles into js and same error is shown
+let testLet = 'Hello';
+
+// REST PARAMETER
+function listAllFruits(...fruitList) {
+   fruitList.forEach(f => {
+     console.log('>> fruit >>', f);
+   });
+}
+listAllFruits('apple', 'banana', 'peach');
+// rest parameter should be last in the parameters
+// function listAllFruits(like, dislike, ...fruitList)
+
+// DESCTUCTURING
+let fruits = ['apple', 'banana', 'cherry', 'peach'];
+const [apple, banana] = fruits;
+console.log('>> destructuring fruits >>', apple + ' - ' + banana );
+
+const [a, ...allFruits] = fruits;
+console.log('>> destructuring fruits >>', 'i like ' + a + ' but not ' + allFruits  );
+
+// default value
+let [ a=1, b=2 ] = [1];
+console.log('>> destructuring default value >>', 'a -> ' + a + ' b -> ' + b );
